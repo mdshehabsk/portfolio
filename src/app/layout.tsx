d079e341449c54ce4da91cb294ea53037ea1da44
Md shehab Uddin <85302877+mdshehabsk@users.noreply.github.com>
2025-01-27 13:04:44 +0600

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/linux.png" type="image/png" />
+      </head>
       <body
         className={` light antialiased`}
       >
